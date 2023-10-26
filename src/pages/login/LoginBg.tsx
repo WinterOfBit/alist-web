07abee2de2ba23c7216d64546f8e69d20326364a
WintBit <wintbit@gmail.com>
2023-10-27 04:07:00 +0800

    feat: scutbot login bg

--- src/pages/login/LoginBg.tsx
+++ src/pages/login/LoginBg.tsx
@@ -1,47 +1,16 @@
-import { Box, useColorModeValue } from "@hope-ui/solid"
-import CornerBottom from "./CornerBottom"
-import CornerTop from "./CornerTop"
+import { Image } from "@hope-ui/solid"
 
 const LoginBg = () => {
-  const bgColor = useColorModeValue("#a9c6ff", "#062b74")
   return (
-    <Box
-      bgColor={bgColor()}
-      pos="fixed"
+    <Image
       top="0"
       left="0"
       overflow="hidden"
       zIndex="-1"
       w="100vw"
-      h="100vh"
-    >
-      <Box
-        pos="absolute"
-        right={{
-          "@initial": "-100px",
-          "@sm": "-300px",
-        }}
-        top={{
-          "@initial": "-1170px",
-          "@sm": "-900px",
-        }}
-      >
-        <CornerTop />
-      </Box>
-      <Box
-        pos="absolute"
-        left={{
-          "@initial": "-100px",
-          "@sm": "-200px",
-        }}
-        bottom={{
-          "@initial": "-760px",
-          "@sm": "-400px",
-        }}
-      >
-        <CornerBottom />
-      </Box>
-    </Box>
+      position="fixed"
+      src="/images/login-bg.jpg"
+    />
   )
 }
 
